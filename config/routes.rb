7afa142be7dded9a378b3aecdac0594ec7e023a1
鈴木由将 <yoshimasa-suzuki@suzukiyushounoMacBook-puro.local>
2018-11-06 01:11:44 +0900

    view setting without design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'timelines/index'
+  get 'timelines/show'
   devise_for :users
   root 'pages#index'
   get 'pages/show'
+
+  resources :timelines
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
