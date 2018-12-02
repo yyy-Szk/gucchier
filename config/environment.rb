3a2565ef8f7860545379ffafe9041f42241405e7
Yoshimasa Suzuki <cognac0004@gmail.com>
2018-12-02 19:47:10 +0900

    devise mailer setting

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => "yourSendGridname you get from heroku",
+  :password => "yourSendGridname you get from heroku",
+  :domain => "heroku.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
